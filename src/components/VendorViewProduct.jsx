8c3bc3c2a04d1e05247a23bc74bb15954281b6d1
Gayaz095 <md.gayazahmed95@gmail.com>
2025-07-02 15:52:27 +0530

    updated code

--- src/components/VendorViewProduct.jsx
+++ src/components/VendorViewProduct.jsx
@@ -34,13 +34,9 @@ const VendorViewProduct = ({
   const childCategory = childCategories.find(
     (child) => child._id === product.childCategoryId
   );
-  if (
-    !mainCategories.length ||
-    !subCategories.length ||
-    !childCategories.length
-  ) {
-    return <div className="view-modal-content">Loading categories...</div>;
-  }
+if (!mainCategories.length || !subCategories.length || !childCategories.length) {
+  return <div className="view-modal-content">Loading categories...</div>;
+}
   return (
     <div className="view-modal-overlay">
       <div className="view-modal-content">
